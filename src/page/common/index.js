fcd4650a8cad39e55fcec8f32a8b4fd0da45769b
power17 <1410837981@qq.com>
2017-12-27 23:52:33 +0800

    initial

---
+++ src/page/common/index.js
@@ -0,0 +1,7 @@
+/*
+* @Author: ASUS
+* @Date:   2017-12-24 22:54:26
+* @Last Modified by:   ASUS
+* @Last Modified time: 2017-12-24 22:54:41
+*/
+'use strict';
---
+++ src/page/index/index.js
@@ -0,0 +1,8 @@
+/*
+* @Author: ASUS
+* @Date:   2017-12-24 22:12:43
+* @Last Modified by:   ASUS
+* @Last Modified time: 2017-12-24 23:01:46
+*/
+'use strict';
+require('./index.css');
